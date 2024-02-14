504dc897ac6a4a0a99338c9a69b695de73c28aab
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-02-14 06:38:24 +0000

    各ページの表示確認

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/about' => "homes#about"
   devise_for :admins
-  devise_for :users
+  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
