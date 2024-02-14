e12914873484d7d71e826bc37a09e030ca2e67d0
EC2 Default User <saki120428@gmail.com>
2024-02-14 04:47:41 +0000

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :orders, only: [:show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :order_details, only: [:update]
+    root to: "admins#top"
+  end
+  resources :adresses, only: [:index, :edit, :create, :update, :destroy]
+  resources :orders, only: [:new, :thanks, :index, :show, :confirm, :create]
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :customers, only: [:unsubscribe, :withdraw]
+  get 'customers/my_page' => "customers#show"
+  get 'customers/information/edit' => "customers#edit"
+  get 'customers/information' => "customers#update"
+ resources :items, only: [:index, :show]
+  root to: 'homes#top'
+  get '/about' => "homes#about"
+  devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
