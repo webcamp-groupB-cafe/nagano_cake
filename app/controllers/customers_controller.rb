9c84cccb8b61062edec5f5fb4b5e3502eedf7aad
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-02-15 05:58:56 +0000

    Add my_page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,8 @@
 class CustomersController < ApplicationController
+  # before_action :authenticate_customer!
+
   def show
+    @customer = current_customer
   end
 
   def edit
@@ -7,4 +10,11 @@ class CustomersController < ApplicationController
 
   def unsubscribe
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
 end
