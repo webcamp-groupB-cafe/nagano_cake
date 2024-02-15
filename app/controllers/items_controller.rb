d2c963cf96adb3c4ebf6d62a7ea7468e08d75916
EC2 Default User <saki120428@gmail.com>
2024-02-15 06:16:16 +0000

    カート機能途中です

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,11 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item =CartItem
+  end
+  
+  private
+  def item_params
+    params.require(:items).permit(:genre_id,:name,:introduction, :nontax_price, :image, :sele_status)
   end
 end
