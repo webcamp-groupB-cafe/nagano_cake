8957d3c6a10138b3cf7bbd47a1b61409f2d90d8a
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-02-15 09:11:46 +0000

    会員情報編集途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 Rails.application.routes.draw do
-   
-  
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  
- root to: 'homes#top'
-  get '/about' => "homes#about"
   namespace :admin do
     resources :orders, only: [:show, :update]
     resources :customers, only: [:index, :show, :edit, :update]
@@ -26,19 +9,15 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   end
   resources :adresses, only: [:index, :edit, :create, :update, :destroy]
   resources :orders, only: [:new, :thanks, :index, :show, :confirm, :create]
-  resources :cart_items, only: [:index, :update, :destroy, :create] do
-      collection do 
-        delete "destroy_all"
-      end 
-    end
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
   resources :customers, only: [:unsubscribe, :withdraw]
   get 'customers/my_page' => "customers#show"
   get 'customers/information/edit' => "customers#edit"
   get 'customers/information' => "customers#update"
  resources :items, only: [:index, :show]
-
-#cd nagano_cake
-#rails routes
+  root to: 'homes#top'
+  get '/about' => "homes#about"
+  devise_for :admins
+  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
