4a4b745eb1e7b7ab18d2b317264c7598bf73d5b4
EC2 Default User <saki120428@gmail.com>
2024-02-16 09:07:45 +0000

    ブランチ統合

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,12 +8,14 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
-    customer = Customer.find(params[:id])
-    customer.update(customer_params)
-    redirect_to admin_customer_path(customer.id)
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
   end
   
+private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
+  
 end
