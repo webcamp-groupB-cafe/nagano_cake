9ad369ce4e4c31ecf3839c499545ad3c4dc79ff6
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-02-17 14:17:34 +0000

    販売ステータス修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,7 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :genre
   has_one_attached :image
-  #販売ステータスに関する記述
-  enum status: { on_sale: 0, off_sale: 1 }
-    
+
   def with_tax_price
     (price*1.1).floor
   end
