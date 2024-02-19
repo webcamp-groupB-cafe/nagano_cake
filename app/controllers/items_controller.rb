36a2fac48e589814228923401bd13de968c48a93
EC2 Default User <saki120428@gmail.com>
2024-02-19 12:19:18 +0000

    商品レイアウト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.page(params[:page]).per(5)
+    @items = Item.all.page(params[:page]).per(6)
   
     @genres = Genre.all
   end
