7f6bd5f6c4eab23aa48170c13f5cb2077bc65736
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-02-20 09:17:31 +0000

    検索窓

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -4,10 +4,13 @@ class Admin::SearchesController < ApplicationController
     @word = params[:word]
     @search = params[:search]
     @range = params[:range]
+    
 
     if @range == "商品"
       @items = Item.looks(@search, @word)
       @genres = Genre.all
+    else 
+      @customers = Customer.looks(@search, @word)
     end
   end
 end
