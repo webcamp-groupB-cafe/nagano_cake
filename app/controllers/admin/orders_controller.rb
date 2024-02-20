dd2a3d481d6bd76974642c99afdf9bcb59e24469
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-02-20 06:20:29 +0000

    管理者注文履歴作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,26 @@
 class Admin::OrdersController < ApplicationController
   def show
-      @order = Order.order
-  if @order.nil?
-render json: { error: "指定された注文が見つかりません" }, status: :not_found
-  end
+    @order_details = OrderDetail.where(order_id: params[:id])
+    @order = Order.find(params[:id])
   end
 
+def update
+  @order_detail = Order.find(params[:id])
+  if @order_detail.update(order_params)
+    redirect_to admin_order_path
+  else
+    redirect_to admin_order_path
+  end
 
+end
+
+private
+
+def order_params
+  params.require(:order).permit(:status)
+end
+
+def order_detail_params
+  params.require(:order_detail).permit(:making_status)
+end
 end
