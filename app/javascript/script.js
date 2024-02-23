581d22a5522d08e7987bed5416ebe3a2f2c0a93b
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-02-23 03:37:52 +0000

    スライダー作成

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -22,3 +22,22 @@ $(document).on('turbolinks:load', function(){
         $window.trigger('scroll');
     });
 });
+
+
+
+// Swiperのオプションを定数化
+const opt = {
+  loop: true, 
+  pagination: { 
+    el: '.swiper-pagination', 
+  },
+  navigation: { 
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+}
+
+// Swiperを実行(初期化)
+$(document).on('turbolinks:load', function() {
+    let swiper = new Swiper('.swiper',opt);
+});
